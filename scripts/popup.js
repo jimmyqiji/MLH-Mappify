8814e5b6e16ee5260d35d25a0ae7606d5115d420
Jimmy Qi <jimmyqiji@hotmail.com>
2018-07-27 04:15:52 -0400

    add scraper

--- scripts/popup.js
+++ scripts/popup.js
@@ -14,3 +14,51 @@ chrome.storage.local.get("mlh", function(data) {
 	}
 	else);
 
+function scrape() {
+	let cheerio = require('cheerio');
+	let jsonframe = require('jsonframe-cheerio');
+
+
+	let $ = cheerio.load('https://mlh.io/seasons/na-2019/events');
+	jsonframe($); // initializes the plugin
+
+
+	var frame = {
+		"hackathons": {           
+			"selector": ".event .event-wrapper .event-link",   
+			"data": [{
+				"name": {
+					 "selector": "[itemprop=url]",
+					 "attr": "title"
+				},
+				"url": {
+					"selector": "[itemprop=url]",
+					"attr": "href"
+				},
+				"logo": {
+					"selector": ".inner .event-logo img",
+					"attr": "src"
+				},
+				"duration": {
+					"selector": ".inner"
+					"data": {
+						"start-date": "[itemprop=startDate]@content",
+						"end-date": "[itemprop=endDate]@content"
+					}
+				},
+				"location": {
+					"selector": ".inner [itemprop=address]",
+					"data": {
+						"province": "[itemprop=addressRegion]",         
+						"city": "[itemprop=addressLocality]"
+					}
+				}
+			}]
+		}
+
+	};
+
+	var companiesList = $('.container.feature.row').scrape(frame);
+	List = $('.list.items').scrape(frame);
+	console.log(companiesList);
+}
