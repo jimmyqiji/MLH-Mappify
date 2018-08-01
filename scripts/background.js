07555b58d08cd73eb5b17a4ad04f3d5900445799
Jimmy Qi <jimmyqiji@hotmail.com>
2018-08-01 00:33:39 -0400

    fix onUpdated listener and callback

--- scripts/background.js
+++ scripts/background.js
@@ -3,18 +3,42 @@ chrome.runtime.onInstalled.addListener(function() {
   });
 
 chrome.tabs.onActivated.addListener(function(info) {
-
 	chrome.tabs.query({
 	    active: true,
 	    lastFocusedWindow: true
+	    // currentWindow: true
 	}, function(array_of_Tabs) {
 	    // Since there can only be one active tab in one active window, 
 	    //  the array has only one element
 	    var tab = array_of_Tabs[0];
 	    var url = tab.url;
 
-	    // "matches": ["*://mlh.io/seasons/na*/events*"],
-	    mlh_bool = /[.]*:\/\/mlh\.io\/seasons\/na-20\d\d\/events[.]*/.test(url);
+	    // "matches": ["*://mlh.io/seasons/*-20*/events*"],
+	    var mlh_bool = /[.]*:\/\/mlh\.io\/seasons\/na-20\d\d\/events[.]*/.test(url);
+	    // alert(mlh_bool);
+	    // alert(url);
+	    if (mlh_bool === true){
+	    	mlh_success();
+	    }
+	    else {
+	    	mlh_failure();
+	    }
+	});
+})
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	chrome.tabs.query({
+	    active: true,
+	    lastFocusedWindow: true
+	    // currentWindow: true
+	}, function(array_of_Tabs) {
+	    // Since there can only be one active tab in one active window, 
+	    //  the array has only one element
+	    var tab = array_of_Tabs[0];
+	    var url = tab.url;
+
+	    // "matches": ["*://mlh.io/seasons/*-20*/events*"],
+	    var mlh_bool = /[.]*:\/\/mlh\.io\/seasons\/na-20\d\d\/events[.]*/.test(url);
 	    // alert(mlh_bool);
 	    // alert(url);
 	    if (mlh_bool === true){
--- scripts/scrape.js
+++ scripts/scrape.js
@@ -5,60 +5,81 @@ var request = require('request');
 document.addEventListener('DOMContentLoaded', function() {
   	var mappifyButton = document.getElementById('btnMappify');
   	mappifyButton.addEventListener('click', function() {
-		var hackathonList = scrape();
-		console.log(hackathonList);
-		mappify(hackathonList);
+  		// var hackathonList = [];
+		// hackathonList = scrape()
+		scrape()
+		.then(function(hackathonList) {
+			console.log("print in .then");
+			console.log(hackathonList);
+			if (hackathonList != "Failed") {
+				mappify(hackathonList);
+			}
+		})
   	})
 });
 
 function scrape() {
-	request('https://mlh.io/seasons/na-2019/events', function (error, response, body) { // tentative, change later to current url
-		if (!error && response.statusCode == 200) {
-			// console.log(body);
-			var $ = cheerio.load(body); 
-			jsonframe($); // initializes the plugin
+	return new Promise(function(resolve, reject) {
+		request('https://mlh.io/seasons/na-2019/events', function (error, response, body) { // tentative, change later to current url
+			if (!error && response.statusCode == 200) {
+				// console.log(body);
+				var $ = cheerio.load(body); 
+				jsonframe($); // initializes the plugin
 
-			var frame = {
-				"hackathons": {           
-					"_s": ".event",   
-					"_d": [{
-						"name": "[itemprop=name]",
-						"url": "[itemprop=url] @ href",
-						"logo": ".event-logo img @ src",
-						"duration": {
-							"_s": ".event-wrapper .event-link .inner",
-							"_d": [{
-								"start-date": "[itemprop=startDate] @ content",
-								"end-date": "[itemprop=endDate] @ content"
-							}]
-						},
-						"location": {
-							"_s": ".event-wrapper .event-link .inner [itemprop=address]",
-							"_d": [{
-								"province": "span[itemprop=addressRegion]",         
-								"city": "[itemprop=addressLocality]"
-							}]
-						}
-					}]
+				var frame = {
+					"hackathons": {
+						"_s": ".event",
+						"_d": [{
+							"name": "[itemprop=name]",
+							"url": "[itemprop=url] @ href",
+							"logo": ".event-logo img @ src",
+							"duration": {
+								"_s": ".event-wrapper .event-link .inner",
+								"_d": [{
+									"start-date": "[itemprop=startDate] @ content",
+									"end-date": "[itemprop=endDate] @ content"
+								}]
+							},
+							"location": {
+								"_s": ".event-wrapper .event-link .inner [itemprop=address]",
+								"_d": [{
+									"province": "span[itemprop=addressRegion]",         
+									"city": "[itemprop=addressLocality]"
+								}]
+							}
+						}]
+					}
+
+				};
+				
+				var hackathonList = [];
+
+				if(hackathonList = $('body').scrape(frame, { string: true })){
+					resolve(hackathonList);
 				}
-
-			};
-
-			var hackathonList = $('body').scrape(frame, { string: true });
-			// alert(hackathonList);
-			// console.log(hackathonList);
-			return hackathonList;
-		}
-		else {
-			console.log(error);
-		}
-	});
+				else {
+					reject("Failed");
+				}
+				
+				console.log("After resolve reject print:")
+				console.log(hackathonList);
+				// return hackathonList;
+				
+			}
+			else {
+				console.log(error);
+			}
+		});
+	})
+	
 }
 
 function mappify(hackathonList) {
 	// $('head').append(leaflet);
 	// $('body').append('<p>appended</p>');
+	console.log("in mappify");
+	console.log(hackathonList);
 	chrome.tabs.executeScript({
-	    file: './scripts/appendtest.js'
+	    file: './scripts/inject.js'
 	  }); 
 }
