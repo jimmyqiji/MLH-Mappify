a056d2c475d720eb13c9625c1ece38453040ddc6
Jimmy Qi <jimmyqiji@hotmail.com>
2018-08-02 00:40:38 -0400

    init map (first time it's displayedgit add . yay for milestone reached)

--- scripts/inject.js
+++ scripts/inject.js
@@ -13,14 +13,15 @@
 
 // map
 $('body').append('<div id="mapid"></div>');
-$('head').append('<style type="text/css"> #mapid { height: 180px; } </style>');
+$('head').append('<style type="text/css"> #mapid { height: 360px; } </style>');
 
 
-// $('head').promise().done(function() {
+$('head').promise().done(function() {
 	// var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 	$('head').append("<script>var mymap = L.map('mapid').setView([51.505, -0.09], 13);</script>");
-// })
+	$('head').append("<script>L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', { attribution: 'Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery © <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>', maxZoom: 18, id: 'mapbox.streets', accessToken: 'pk.eyJ1IjoicWl2YWxyeSIsImEiOiJjampnNTBhY2s1NHRxM3BvZ2U1eDN3anQyIn0.qpmZ6Dv3v0lAjWVhGEgvig'}).addTo(mymap);</script>");
+});
 
 
 //test
-$('body').append('<div style="position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000;"></div>');
+// $('body').append('<div style="position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000;"></div>');
