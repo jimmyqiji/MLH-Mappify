f170554a4e44a5f856e203e4f5a4f2590d722b02
Jimmy Qi <jimmyqiji@hotmail.com>
2018-08-03 00:56:32 -0400

    add .main-side-bar

--- scripts/inject.js
+++ scripts/inject.js
@@ -12,13 +12,27 @@
 // document.head.appendChild(leafletjs);
 
 // map
-$('body').append('<div id="mapid"></div>');
-$('head').append('<style type="text/css"> #mapid { height: 360px; } </style>');
+// $('body > div:nth-child(1)').addClass("main-side-bar");
+$('body .footer').remove();
+$('body .container').addClass("main-side-bar");
+$('head').append("<style> .main-side-bar { position: absolute; left: 0px; top: 0px; width: 300px; z-index: 99; } </style>");
+// $('.mlh-navigation .navbar-nav').addClass("main-side-bar");
+// $('head').append("<style> .nav {padding-left: 0;list-style: none;} </style>");
+$('body .event').removeClass("col-lg-3 col-md-4 col-sm-6");
+// $('head').append("<style>body .event { display: block; }");
+$('html').attr('style','background-color:rgba(0, 0, 0, 0);');
+// $('html').addClass("main-side-bar");
+$('body .btn-yellow').remove();
+$('body .btn-blue').remove();
+$('head').append('<style> body { background: transparent; } </style>');
+// $('body .navbar-header, body .navbar, body .navbar-default,').attr('style', 'height: 50px;')
 
+// $('body .navbar-toggle').attr('style','position: relative; float: right; margin-right: 15px; padding: 9px 10px; margin-top: 8px; margin-bottom: 8px; background-color: transparent; background-image: none; border: 1px solid transparent; border-radius: 4px;}');
 
 $('head').promise().done(function() {
-	// var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-	$('head').append("<script>var mymap = L.map('mapid').setView([51.505, -0.09], 13);</script>");
+	$('body').append('<div id="mapid"></div>');
+	$('head').append('<style type="text/css"> #mapid { position: fixed; height: 100%; top: 0; left: 0; } </style>');
+	$('head').append("<script>var mymap = L.map('mapid').setView([43.482670, -80.250168], 10);</script>");
 	$('head').append("<script>L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', { attribution: 'Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery © <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>', maxZoom: 18, id: 'mapbox.streets', accessToken: 'pk.eyJ1IjoicWl2YWxyeSIsImEiOiJjampnNTBhY2s1NHRxM3BvZ2U1eDN3anQyIn0.qpmZ6Dv3v0lAjWVhGEgvig'}).addTo(mymap);</script>");
 });
 
