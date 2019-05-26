e01263562c336b1980325c71d123559a20a13d92
Jimmy <jimmyqiji@hotmail.com>
2019-05-26 16:20:08 -0400

    update inject.js to echo mlh layout change

--- scripts/inject.js
+++ scripts/inject.js
@@ -4,76 +4,70 @@ var request = require('request');
 
 scrape()
 .then(function(hackathonList) {
-	// console.log("print in .then");
-	// console.log(hackathonList);
-	if (hackathonList != "Failed") {
-		mappify(hackathonList);
-	}
+    // console.log("print in .then");
+    // console.log(hackathonList);
+    if (hackathonList != "Failed") {
+        mappify(hackathonList);
+    }
 })
 
 function scrape() {
-	return new Promise(function(resolve, reject) {
-		request('https://mlh.io/seasons/na-2019/events', function (error, response, body) { // tentative, change later to current url
-			if (!error && response.statusCode == 200) {
-				// console.log(body);
-				var $ = cheerio.load(body); 
-				jsonframe($); // initializes the plugin
+    return new Promise(function(resolve, reject) {
+        request('https://mlh.io/seasons/na-2019/events', function (error, response, body) { // tentative, change later to current url
+            if (!error && response.statusCode == 200) {
+                // console.log(body);
+                var $ = cheerio.load(body);
+                jsonframe($); // initializes the plugin
 
-				var frame = {
-					"hackathons": {
-						"_s": ".event",
-						"_d": [{
-							"name": "[itemprop=name]",
-							"url": "[itemprop=url] @ href",
-							"logo": ".event-logo img @ src",
-							"duration": {
-								"_s": ".event-wrapper .event-link .inner",
-								"_d": [{
-									"start-date": "[itemprop=startDate] @ content",
-									"end-date": "[itemprop=endDate] @ content"
-								}]
-							},
-							"location": {
-								"_s": ".event-wrapper .event-link .inner [itemprop=address]",
-								"_d": [{
-									"province": "span[itemprop=addressRegion]",         
-									"city": "[itemprop=addressLocality]"
-								}]
-							}
-						}]
-					}
+                var frame = {
+                    "hackathons": {
+                        "_s": ".event",
+                        "_d": [{
+                            "name": "[itemprop=name]",
+                            "url": "[itemprop=url] @ href",
+                            "logo": ".event-logo img @ src",
+                            "duration": {
+                                "_s": ".event-wrapper .event-link .inner",
+                                "_d": [{
+                                    "start-date": "[itemprop=startDate] @ content",
+                                    "end-date": "[itemprop=endDate] @ content"
+                                }]
+                            },
+                            "location": {
+                                "_s": ".event-wrapper .event-link .event-location",
+                                "_d": [{
+                                    "province": "[itemprop=state]",        
+                                    "city": "[itemprop=city]"
+                                }]
+                            }
+                        }]
+                    }
+                };
+                
+                var hackathonList;
 
-				};
-				
-				var hackathonList;
-
-				if(hackathonList = $('body').scrape(frame, { string: true })){
-					resolve(hackathonList);
-				}
-				else {
-					reject("Failed");
-				}
-				
-				// console.log("After resolve reject print:")
-				// console.log(hackathonList);
-				// return hackathonList;
-				
-			}
-			else {
-				console.log(error);
-			}
-		});
-	})
-	
+                if(hackathonList = $('body').scrape(frame, { string: true })){
+                    resolve(hackathonList);
+                }
+                else {
+                    reject("Failed");
+                }
+            }
+            else {
+                console.log(error);
+            }
+        });
+    })
+    
 }
 
 function mappify(hackathonList) {
-	hackathonJson = JSON.parse(hackathonList);
-	console.log(hackathonJson);
-	var province = hackathonJson.hackathons[0].location[0].province;
-	var city = hackathonJson.hackathons[0].location[0].city;
-	console.log(province);
-	console.log(city);
+    hackathonJson = JSON.parse(hackathonList);
+    console.log(hackathonJson);
+    var province = hackathonJson.hackathons[0].location[0].province;
+    var city = hackathonJson.hackathons[0].location[0].city;
+    console.log(province);
+    console.log(city);
 }
 
 $('head').append("<style> html { overflow-y: hidden; } </style>");
@@ -81,12 +75,8 @@ $('body > div:nth-child(1)').addClass("old-site");
 $('head').append("<style> .old-site { max-height: 77px; } </style>");
 $('body > div:nth-child(1) > div:nth-child(6) > div:nth-child(1)').remove();
 $('body .footer').remove();
-// $('.collapse').removeClass("collapse");
-// $('.navbar-toggle').remove();
-// $('.navbar-default').attr("style", 'content:" "; display:table;');
 
 var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-// var navh = 
 var mapheight = h - 50;
 var mapstyle = '<style type="text/css"> #mapid { position: fixed; width:100%; height:' + mapheight.toString() + 'px; bottom: 0px; right: 0; z-index: 50;} </style>';
 var sideBarStyle = '<style> .main-side-bar { position: absolute; left: 0px; bottom: 0px; width: 20%; height: ' + mapheight.toString() + 'px; overflow-y: auto; padding-left: 40px; padding-top: 60px; padding-right: 40px; z-index: 99; min-width: 320px; background-color: white;} </style>';
@@ -99,20 +89,14 @@ $('body .btn-yellow').remove();
 $('body .btn-blue').remove();
 $('head').append('<style> .old-layout { background: white; } </style>');
 
-// $('body .navbar-toggle').attr('style','position: relative; float: right; margin-right: 15px; padding: 9px 10px; margin-top: 8px; margin-bottom: 8px; background-color: transparent; background-image: none; border: 1px solid transparent; border-radius: 4px;}');
-// $('head').append('<style> .collapse {display: none;} </style>');
-// $('head').append('<style> .navbar {position: relative;min-height: 50px;margin-bottom: 0;border: 1px solid transparent;}');
-
 $('head').promise().done(function() {
-	$('body').append('<div id="mapid"></div>');
-	// using width:100 and height:100 together fixed the 0 height problem
-	// $('head').append('<style type="text/css"> #mapid { position: relative; width:100%; height:100%; top: 0; left: 0; z-index: 50;} </style>');
-	$('head').append(mapstyle);
-	$('head').append("<script>var map = L.map('mapid').setView([43.482670, -80.250168], 10);</script>");
-	$('head').append("<script>L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', { attribution: 'Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery © <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>', maxZoom: 18, id: 'mapbox.streets', accessToken: 'pk.eyJ1IjoicWl2YWxyeSIsImEiOiJjampnNTBhY2s1NHRxM3BvZ2U1eDN3anQyIn0.qpmZ6Dv3v0lAjWVhGEgvig'}).addTo(map);</script>");
-	$('head').append("<script>map.zoomControl.setPosition('topright');</script>");
+    $('body').append('<div id="mapid"></div>');
+    // using width:100 and height:100 together fixed the 0 height problem
+    // $('head').append('<style type="text/css"> #mapid { position: relative; width:100%; height:100%; top: 0; left: 0; z-index: 50;} </style>');
+    $('head').append(mapstyle);
+    $('head').append("<script>var map = L.map('mapid').setView([43.482670, -80.250168], 10);</script>");
+    $('head').append("<script>L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', { attribution: 'Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery © <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>', maxZoom: 18, id: 'mapbox.streets', accessToken: 'pk.eyJ1IjoicWl2YWxyeSIsImEiOiJjampnNTBhY2s1NHRxM3BvZ2U1eDN3anQyIn0.qpmZ6Dv3v0lAjWVhGEgvig'}).addTo(map);</script>");
+    $('head').append("<script>map.zoomControl.setPosition('topright');</script>");
 });
 
 
-//test
-//$('body').append('<div style="position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000;"></div>');
