9fc14ff126b2b62644dec2258f145806f495460b
Jimmy <jimmyqiji@hotmail.com>
2019-06-07 20:32:06 -0400

    clean up background.js

--- scripts/background.js
+++ scripts/background.js
@@ -1,53 +1,40 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.local.clear();
-  });
+	chrome.storage.local.clear();
+});
+
+function choose_popup(array_of_Tabs) {
+	// Since there can only be one active tab in one active window, the array has only one element
+	var tab = array_of_Tabs[0];
+	var url = tab.url;
+
+	// "matches": ["*://mlh.io/seasons/*-20*/events*"],
+	var mlh_bool = /[.]*:\/\/mlh\.io\/seasons\/na-20\d\d\/events[.]*/.test(url);
+	if (mlh_bool === true){
+		mlh_success();
+	}
+	else {
+		mlh_failure();
+	}
+}
 
 chrome.tabs.onActivated.addListener(function(info) {
-	chrome.tabs.query({
+	chrome.tabs.query(
+		{
 	    active: true,
 	    lastFocusedWindow: true
-	    // currentWindow: true
-	}, function(array_of_Tabs) {
-	    // Since there can only be one active tab in one active window, 
-	    //  the array has only one element
-	    var tab = array_of_Tabs[0];
-	    var url = tab.url;
-
-	    // "matches": ["*://mlh.io/seasons/*-20*/events*"],
-	    var mlh_bool = /[.]*:\/\/mlh\.io\/seasons\/na-20\d\d\/events[.]*/.test(url);
-	    // alert(mlh_bool);
-	    // alert(url);
-	    if (mlh_bool === true){
-	    	mlh_success();
-	    }
-	    else {
-	    	mlh_failure();
-	    }
-	});
+		},
+		choose_popup
+	);
 })
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	chrome.tabs.query({
+	chrome.tabs.query(
+		{
 	    active: true,
 	    lastFocusedWindow: true
-	    // currentWindow: true
-	}, function(array_of_Tabs) {
-	    // Since there can only be one active tab in one active window, 
-	    //  the array has only one element
-	    var tab = array_of_Tabs[0];
-	    var url = tab.url;
-
-	    // "matches": ["*://mlh.io/seasons/*-20*/events*"],
-	    var mlh_bool = /[.]*:\/\/mlh\.io\/seasons\/na-20\d\d\/events[.]*/.test(url);
-	    // alert(mlh_bool);
-	    // alert(url);
-	    if (mlh_bool === true){
-	    	mlh_success();
-	    }
-	    else {
-	    	mlh_failure();
-	    }
-	});
+		},
+		choose_popup
+	);
 });
 
 function mlh_success() {
